642c4c1194301efb349b9da57f126879bc6f09dc
Sebastian Giupana <sebastiangiupana@Sebastians-MacBook-Air.local>
2025-10-31 16:06:41 +0100

    prismaaaa

--- apps/server/src/index.js
+++ apps/server/src/index.js
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import prisma from "./model.js";
+
 dotenv.config();
 
 const PORT = process.env.PORT;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Express Server");
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send(users);
 });
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🥦 Server is legendary on http://localhost:${PORT}`);
 });
